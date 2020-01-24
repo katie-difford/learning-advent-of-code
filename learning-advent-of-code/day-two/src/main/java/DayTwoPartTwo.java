2f60c824f3bce6479a327279f0e094ca1f15de43
Team NIM <nim.team@sky.uk>
2020-01-24 14:52:40 +0000

    Katie: Rough idea of what I need to do.

--- learning-advent-of-code/day-two/src/main/java/DayTwoPartTwo.java
+++ learning-advent-of-code/day-two/src/main/java/DayTwoPartTwo.java
@@ -5,30 +5,29 @@ import java.util.stream.Stream;
 public class DayTwoPartTwo {
 
     public static void main(String[] args) throws Exception {
-        final String newContent = Files.readString(Paths.get(DayTwo.class.getResource("newInstructions.txt").toURI()));
+        final String newContent = Files.readString(Paths.get(DayTwoPartOne.class.getResource("newInstructions.txt").toURI()));
         final String[] newSplit = newContent.split(",");
         final int[] newInstructions = Stream.of(newSplit).mapToInt(s -> Integer.parseInt(s)).toArray();
 
         final DayTwoPartTwo dayTwoPartTwo = new DayTwoPartTwo();
-
-        dayTwoPartTwo.nounAndVerbCalculation(newInstructions);
-
+        dayTwoPartTwo.getTheNumbersInPositionOneAndTwoThatReturnTheDesiredValue(newInstructions);
     }
 
-    private int[] nounAndVerbCalculation(final int[] newInstructions) {
-        for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j<=5; j++) {
+    private void setTheNewPositionsInInstructions(int[] newInstructions, int firstNewNumber, int secondNewNumber) {
+        newInstructions[1] = firstNewNumber;
+        newInstructions[2] = secondNewNumber;
+    }
 
-                newInstructions[1] = i;
-                newInstructions[2] = j;
-
-                System.out.println("----------------");
-                System.out.println("New Instruction Index 1: " + newInstructions[1]);
-                System.out.println("New Instruction Index 2: " + newInstructions[2]);
+    private void getTheNumbersInPositionOneAndTwoThatReturnTheDesiredValue(final int[] instructions) {
+        while(instructions[0] != 19690720) {
+            for (int i = 0; i <= 5; i++) {
+                for (int j = 0; j <= 5; j++) {
+                    setTheNewPositionsInInstructions(instructions, i, j);
+                    DayTwo.opcodeCalculation(instructions);
+                }
             }
-        }
-        return newInstructions;
-    }
 
+        }
+    }
 
 }
